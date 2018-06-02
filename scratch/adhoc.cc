47369098eb8111733ff3145de9a1e8cc52782b14
Long Nguyen <nlhlong02@gmail.com>
2018-06-03 00:46:12 +0200

    add flow monitor

--- scratch/adhoc.cc
+++ scratch/adhoc.cc
@@ -1,11 +1,8 @@
 
-#include "ns3/yans-wifi-helper.h"
-#include "ns3/mobility-helper.h"
 #include "ns3/ipv4-address-helper.h"
 #include "ns3/yans-wifi-channel.h"
 #include "ns3/mobility-model.h"
 #include "ns3/olsr-helper.h"
-#include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
 #include "ns3/internet-stack-helper.h"
 #include "ns3/applications-module.h"
@@ -15,82 +12,80 @@
 #include "ns3/internet-module.h"
 #include "ns3/olsr-routing-protocol.h"
 #include "ns3/v4ping-helper.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-helper.h"
+
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("WifiSimpleAdhocGrid");
-
-
-
-void ReceivePacket (Ptr<Socket> socket)
-{
-  while (socket->Recv ())
-    {
-      NS_LOG_UNCOND ("Received one packet!");
-    }
-}
-
-static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
-                             uint32_t pktCount, Time pktInterval )
-{
-  if (pktCount > 0)
-    {
-      socket->Send (Create<Packet> (pktSize));
-      Simulator::Schedule (pktInterval, &GenerateTraffic,
-                           socket, pktSize,pktCount - 1, pktInterval);
-    }
-  else
-    {
-      socket->Close ();
-    }
-}
-
-
+NS_LOG_COMPONENT_DEFINE ("HoangViet-Simulation");
 
 
 int main (int argc, char *argv[])
 {
-  //std::string phyMode ("ErpOfdmRate54Mbps");
-  double distance = 50;  // m
-  uint32_t numNodes = 4;
-  //uint32_t packetSize = 1024;
-  //uint32_t numPackets = 10;
-  //double interval = 1.0;
-  // bool tracing = false;
+  double distance = 5;  // distance of nodes , in "m"
+  //uint32_t numNodes = 4; // number of nodes
+  double udpStart = 1.0; // Start time of UDP App
+  double udpStop = 5.0; // Stop time of UDP App
+  double tcpStart = 6.0; // Start time of TCP App
+  double tcpStop = 10.0;  // Stop time of TCP App
+  double pingStart = 11.0;  // Start time of Ping App
+  double pingStop = 15.0;  // Stop time of Ping App
+  double simStop = 30.0;  // Stop time of Simulator
   bool verbose = false;
-  bool tracing = true;
+  bool enPing = false;
+  bool enTcp = false;
+  bool enUdp = false;
+
+
 
   CommandLine cmd;
-  //cmd.AddValue ("phyMode", "Wifi Phy mode", phyMode);
   cmd.AddValue ("distance", "distance (m)", distance);
-  cmd.AddValue ("numNodes", "number of nodes", numNodes);
+  //cmd.AddValue ("numNodes", "number of nodes", numNodes);
+  cmd.AddValue ("enPing", "enable Ping Application", enPing);
+  cmd.AddValue ("enTcp", "enable TCP Application", enTcp);
+  cmd.AddValue ("enUdp", "enable UDP Application", enUdp);
+  cmd.AddValue ("udpStart", "Start time of UDP", udpStart);
+  cmd.AddValue ("udpStop", "Stop time of UDP", udpStop);
+  cmd.AddValue ("tcpStart", "Start time of TCP", tcpStart);
+  cmd.AddValue ("tcpStop", "Stop time of TCP", tcpStop);
+  cmd.AddValue ("pingStart", "Start time of Ping", pingStart);
+  cmd.AddValue ("pingStop", "Stop time of Ping", udpStop);
+  cmd.AddValue ("simStop", "Stop time of Simulator", simStop);  
+
   cmd.Parse (argc, argv);
 
 
 
-  Time::SetResolution (Time::NS);
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+  //Time::SetResolution (Time::NS);
+ // LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  //LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
 
-
+  // Create 4 Nodes 
   NodeContainer c;
-  c.Create (numNodes);
+  c.Create (4);
+
+  
   WifiHelper wifi;
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211g);
+  wifi.SetRemoteStationManager ("ns3::MinstrelWifiManager");
+
   if (verbose)
     {
       wifi.EnableLogComponents ();  // Turn on all Wifi logging
     }
-
+  
+  // Configure Physical Layer Properties
   YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
-  // set it to zero; otherwise, gain will be added
-  wifiPhy.Set ("RxGain", DoubleValue (1) );
-  //wifiPhy.Set ("txGain", DoubleValue (1) );
-  // ns-3 supports RadioTap and Prism tracing extensions for 802.11b
-  wifiPhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+  wifiPhy.Set ("TxPowerStart", DoubleValue(1.0));
+  wifiPhy.Set ("TxPowerEnd", DoubleValue(1.0));
+  wifiPhy.Set ("RxGain", DoubleValue (1.0));
+  wifiPhy.Set ("TxGain", DoubleValue (1.0));
+  wifiPhy.Set ("ChannelWidth", UintegerValue(20));
 
-
-
+  // Setting up Wifi Channel
   YansWifiChannelHelper wifiChannel;
   wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
   wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
@@ -99,46 +94,39 @@ int main (int argc, char *argv[])
 
   // Add an upper mac and disable rate control
   WifiMacHelper wifiMac;
-  wifi.SetStandard (WIFI_PHY_STANDARD_80211g);
-  wifi.SetRemoteStationManager ("ns3::MinstrelWifiManager");//, "DataMode",StringValue (phyMode), "ControlMode",StringValue (phyMode));
-
-
-  // Set it to adhoc mode
   wifiMac.SetType ("ns3::AdhocWifiMac");
   NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, c);
 
 
 
-  // distance to be set later
+  // Place 4 nodes in a row, the distance between each node is the value of parameter "distance" ( can be change in ./waf --run)
   MobilityHelper mobility;
-  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                 "MinX", DoubleValue (0.0),
-                                 "MinY", DoubleValue (0.0),
-                                 "DeltaX", DoubleValue (distance),
-                                 "DeltaY", DoubleValue (distance),
-                                 "GridWidth", UintegerValue (5),
-                                 "LayoutType", StringValue ("RowFirst"));
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (c);
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject <ListPositionAllocator>();
+  positionAlloc ->Add(Vector(0, 0, 0)); // place node 0 at origin
+  positionAlloc ->Add(Vector(distance, 0, 0)); // place node 1 at "1*distance" (m) from node 0
+  positionAlloc ->Add(Vector(2*distance, 0, 0)); // place node 2 at "2*distance" (m) from node 0
+  positionAlloc ->Add(Vector(3*distance, 0, 0)); // place node 3 at "3*distance" (m) from node 0
+  mobility.SetPositionAllocator(positionAlloc);
+  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+  mobility.Install(c);
+
 
 
   //Enable OLSR
   OlsrHelper olsr;
-  //Ipv4StaticRoutingHelper staticRouting;
   
   Ipv4ListRoutingHelper list;
-  //list.Add (staticRouting, 0);
   list.Add (olsr, 10);
 
 
-
+  // Install OLSR 
   InternetStackHelper internet;
-  internet.SetRoutingHelper (list); // has effect on the next Install ()
+  internet.SetRoutingHelper (list); 
   internet.Install (c);
 
 
 
-
+  // Assgign Adresses to all Devices
   Ipv4AddressHelper ipv4;
   NS_LOG_INFO ("Assign IP Addresses.");
   ipv4.SetBase ("10.1.1.0", "255.255.255.0");
@@ -146,96 +134,91 @@ int main (int argc, char *argv[])
 
 
 
-  ////// PING ///////////
-  V4PingHelper icmp(i.GetAddress (3));  // destination address of icmp packets is node 3
-  ApplicationContainer pingapp = icmp.Install(c.Get (0));  // Source of icmp packets
-  pingapp.Start (Seconds (11.0));
-  pingapp.Stop (Seconds (15.0));
+  // Install Ping Application sent from node 0 to node 3
+  if (enPing)
+  {
+    V4PingHelper icmp(i.GetAddress (3));  // destination address of icmp packets is node 3
+    ApplicationContainer pingApp = icmp.Install(c.Get (0));  // Source of icmp packets
+    pingApp.Start (Seconds (pingStart));  // Start time of Ping App
+    pingApp.Stop (Seconds (pingStop));   // Stop time of Ping App
+  }
+
+
+
+  
+  // Install TCP Application sent from node 0 to node 3
+  // Create a BulkSendApplication and install it on node 0
+  
+  if (enTcp)
+  {
+    uint16_t tcpport = 9;  // well-known echo port number
+
+    BulkSendHelper source ("ns3::TcpSocketFactory",
+                         InetSocketAddress (i.GetAddress (3), tcpport)); // socket address of destination, node 3 in this case
+    source.SetAttribute ("MaxBytes", UintegerValue (0));  // Set the amount of data to send in bytes.  Zero is unlimited.
+    ApplicationContainer tcpSourceApp = source.Install (c.Get (0)); // Install the Source App Container on node 0
+    tcpSourceApp.Start (Seconds (tcpStart)); // Start time of tcp source
+    tcpSourceApp.Stop (Seconds (tcpStop));  // Stop time of tcp source
+
+    // Create a PacketSinkApplication and install it on node 3
+
+    PacketSinkHelper sink ("ns3::TcpSocketFactory",
+                         InetSocketAddress (Ipv4Address::GetAny (), tcpport));  // socket address of senders
+    ApplicationContainer tcpSinkApp = sink.Install (c.Get (3));  // Install the Sink App Container on node 3
+    tcpSinkApp.Start (Seconds (tcpStart));	// Start time of tcp sink
+    tcpSinkApp.Stop (Seconds (tcpStop));     // Stop time of tcp sink
+  }
+
+
+  if (enUdp)
+  {
+
+    uint16_t port = 4000;
+    UdpServerHelper server (port);
+    ApplicationContainer udpApp = server.Install (c.Get (3));
+    udpApp.Start (Seconds (udpStart));
+    udpApp.Stop (Seconds (udpStop));
+
+
+    uint32_t MaxPacketSize = 1472;
+    uint32_t MaxPackets = 1000000;
+    Time interPacketInterval = Seconds (0.00066);
+    UdpClientHelper udpClient (i.GetAddress(3), port);
+    udpClient.SetAttribute ("Interval", TimeValue (interPacketInterval));
+    udpClient.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
+    udpClient.SetAttribute ("MaxPackets", UintegerValue (MaxPackets));
+    udpApp = udpClient.Install (c.Get (0));
+    udpApp.Start (Seconds (udpStart));
+    udpApp.Stop (Seconds (udpStop));
+
+  } 
+ 
+
+
+
+  // Configure Flow Monitor
+  Ptr<FlowMonitor> flowMonitor;
+  FlowMonitorHelper flowHelper;
+  flowMonitor = flowHelper.InstallAll();
 
 
 
 
 
 
-// Create a BulkSendApplication and install it on node 0////
-//
-  uint16_t tcpport = 9;  // well-known echo port number
+  // Enable Pcap file for Wireshark
+  wifiPhy.EnablePcapAll ("adhoc");
+    
 
-
-  BulkSendHelper source ("ns3::TcpSocketFactory",
-                         InetSocketAddress (i.GetAddress (3), tcpport));
-  // Set the amount of data to send in bytes.  Zero is unlimited.
-  source.SetAttribute ("MaxBytes", UintegerValue (10000));
-  ApplicationContainer sourceApps = source.Install (c.Get (0));
-  sourceApps.Start (Seconds (6.0));
-  sourceApps.Stop (Seconds (10.0));
-
-//
-// Create a PacketSinkApplication and install it on node 3
-//
-  PacketSinkHelper sink ("ns3::TcpSocketFactory",
-                         InetSocketAddress (Ipv4Address::GetAny (), tcpport));
-  ApplicationContainer sinkApps = sink.Install (c.Get (3));
-  sinkApps.Start (Seconds (6.0));
-  sinkApps.Stop (Seconds (11.0));
-
-
-
-//////////////////////////////////////////
-
-
- // Create one udpServer applications on node one.
-//
-  uint16_t port = 4000;
-  UdpServerHelper server (port);
-  ApplicationContainer apps = server.Install (c.Get (3));
-  apps.Start (Seconds (1.0));
-  apps.Stop (Seconds (6.0));
-
-//
-// Create one UdpClient application to send UDP datagrams from node zero to
-// node one.
-//
-  uint32_t MaxPacketSize = 1472;
-  Time interPacketInterval = Seconds (0.05);
-  //uint32_t maxPacketCount = 320;
-  UdpTraceClientHelper client (i.GetAddress(3), port,"");
-  //client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
-  //client.SetAttribute ("Interval", TimeValue (interPacketInterval));
-  client.SetAttribute ("MaxPacketSize", UintegerValue (MaxPacketSize));
-  apps = client.Install (c.Get (0));
-  apps.Start (Seconds (2.0));
-  apps.Stop (Seconds (6.0));
-
-
-
-
- // UdpEchoServerHelper echoServer (9);
-
- // ApplicationContainer serverApps = echoServer.Install (c.Get (3));
- // serverApps.Start (Seconds (1.0));
- // serverApps.Stop (Seconds (10.0));
-
- // UdpEchoClientHelper echoClient (i.GetAddress (3), 9);
-  //echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
- // echoClient.SetAttribute ("Interval", TimeValue (Seconds (0.5)));
- // echoClient.SetAttribute ("PacketSize", UintegerValue (1518));
-//
- // ApplicationContainer clientApps = echoClient.Install (c.Get (0));
- // clientApps.Start (Seconds (2.0));
- // clientApps.Stop (Seconds (10.0));
-
-
-
-  if (tracing == true)
-    {
-      wifiPhy.EnablePcapAll ("adhoc");
-    }
-
-
-  Simulator::Stop(Seconds(16.0));
+  Simulator::Stop(Seconds(simStop));
   Simulator::Run ();
   Simulator::Destroy ();
+  
+
+  // Capture Flow Monitor to Csv File
+  flowMonitor->CheckForLostPackets ();
+  flowMonitor->SerializeToXmlFile("NameOfFile.csv", true, true);
+
   return 0;
 }
 
--- scratch/simulation-task.cc
+++ scratch/simulation-task.cc
@@ -14,23 +14,26 @@ NS_LOG_COMPONENT_DEFINE ("SimulationTask");
 
 
 int main (int argc, char *argv[]) {
-    int nodeNum = 4;
-    std::string mode = "normal";
-    std::string protocol = "udp";
-    int distance = 5;
+    int nodeCount = 4;
+    bool debug = false;
+    bool tcp = true;
+    int distance = 1;
+    DataRate dataRate = DataRate("54Mbps");
+    double simTime = 5.0;
 
     CommandLine cmd;
-    cmd.AddValue ("mode", "Simulation mode (debug, normal)", mode);
-    cmd.AddValue ("app", "Application (udp, tcp)", mode);
-    cmd.AddValue ("nodeNum", "Number of nodes", nodeNum);
+    cmd.AddValue ("debug", "Simulation mode", debug);
+    cmd.AddValue ("tcp", "Protocol", tcp);
+    cmd.AddValue ("nodeCount", "Number of nodes", nodeCount);
     cmd.AddValue ("distance", "Distance between 2 nodes", distance);
+    cmd.AddValue ("simTime", "Simulation time", simTime);
     cmd.Parse (argc, argv);
 
     // Set up a 4-node wireless adhoc 802.11g network.
     NodeContainer nodes;
-    nodes.Create(nodeNum);
+    nodes.Create(nodeCount);
     Ptr<Node> clientNode = nodes.Get(0);
-    Ptr<Node> serverNode = nodes.Get(nodeNum-1);
+    Ptr<Node> serverNode = nodes.Get(nodeCount-1);
     NetDeviceContainer devices;
 
     // Set up wifi channel
@@ -71,73 +74,101 @@ int main (int argc, char *argv[]) {
     addresses.SetBase("10.1.1.0", "255.255.255.0");
     Ipv4InterfaceContainer interfaces = addresses.Assign(devices);
     // Ipv4Address clientAddr = interfaces.GetAddress(0); 
-    Ipv4Address serverAddr = interfaces.GetAddress(nodeNum-1);    
-
-    // Set distance between nodes
-    MobilityHelper mobility;
-    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
-                                  "MinX", DoubleValue (0.0),
-                                  "MinY", DoubleValue (0.0),
-                                  "DeltaX", DoubleValue (distance),
-                                  "GridWidth", UintegerValue (nodeNum),
-                                  "LayoutType", StringValue ("RowFirst"));
-    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-    mobility.Install(nodes);
+    Ipv4Address serverAddr = interfaces.GetAddress(nodeCount-1);    
 
     // Set up application containers
     ApplicationContainer clientApps;
     ApplicationContainer serverApps;
 
-    if (mode == "debug") {
+    ApplicationContainer sourceApps;
+    ApplicationContainer sinkApps;
+
+    if (debug) {
         // Install ping application
         NS_LOG_INFO ("Debug mode. Installing ping application");
         Ipv4Address destAddr = serverAddr;
         V4PingHelper ping(destAddr);
         clientApps = ping.Install(clientNode);
         clientApps.Start (Seconds (1.0));
-        clientApps.Stop (Seconds (10.0));
+        clientApps.Stop (Seconds (simTime));
     }
     else {
         NS_LOG_INFO ("Normal mode");
         uint16_t port;
-        ApplicationContainer sourceApps;
-        ApplicationContainer sinkApps;
         std::string selectedProtocol;
-        if (protocol == "udp") {
-            selectedProtocol = "ns3::UdpSocketFactory";
-            port = 4000;
-            NS_LOG_INFO ("Installing UDP app");
-        }
-        else {
+        if (tcp) {
             selectedProtocol = "ns3::TcpSocketFactory";
             port = 9; 
             NS_LOG_INFO ("Installing TCP app");
+        }
+        else {
+            selectedProtocol = "ns3::UdpSocketFactory";
+            port = 4000;
+            NS_LOG_INFO ("Installing UDP app");
         }
         // Install the apps
         InetSocketAddress destAddr = InetSocketAddress (serverAddr, port);
         InetSocketAddress sinkAddr = InetSocketAddress (Ipv4Address::GetAny(), port);
         OnOffHelper onOff (selectedProtocol, destAddr);
+        onOff.SetConstantRate(DataRate(dataRate));
         sourceApps = onOff.Install (clientNode);
         sourceApps.Start (Seconds (1.0));
-        sourceApps.Stop (Seconds (10.0));
+        sourceApps.Stop (Seconds (simTime));
+
         PacketSinkHelper sink (selectedProtocol, sinkAddr);
-        sourceApps = sink.Install (serverNode);
-        sourceApps.Start (Seconds (1.0));
-        sourceApps.Stop (Seconds (10.0));
+        sinkApps = sink.Install (serverNode);
+        sinkApps.Start (Seconds (1.0));
+        sinkApps.Stop (Seconds (simTime));
     }
 
     // enable PCAP on the channel
-    phy.EnablePcapAll("debug");
+    phy.EnablePcapAll("result");
 
-    // Set up Flow Monitor module 
-    Ptr<FlowMonitor> flowMonitor;
+    // Set distance between nodes
+    Ptr<ListPositionAllocator>pstAlloc = CreateObject <ListPositionAllocator>();
+    for (int i = 0; i < nodeCount; i++) {
+        pstAlloc->Add(Vector(i*distance, 0, 0));
+    }
+    MobilityHelper mobility;
+    mobility.SetPositionAllocator(pstAlloc);
+    // mobility.SetPositionAllocator("ns3::GridPositionAllocator",
+    //                               "MinX", DoubleValue (0.0),
+    //                               "MinY", DoubleValue (0.0),
+    //                               "DeltaX", DoubleValue (distance),
+    //                               "GridWidth", UintegerValue (nodeCount),
+    //                               "LayoutType", StringValue ("RowFirst"));
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(nodes);
+
     FlowMonitorHelper flowHelper;
-    flowMonitor = flowHelper.InstallAll();
+    Ptr<FlowMonitor> monitor= flowHelper.InstallAll();
 
+    // Run simulation
     NS_LOG_INFO ("Run Simulation.");
-    Simulator::Stop(Seconds(10.0));
+    Simulator::Stop(Seconds(simTime + 5));
     Simulator::Run ();
-    flowMonitor->SerializeToXmlFile("throughputAnalysis.csv", true, true);
+
+    // Calculate statistical numbers
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats ();
+    double offeredLoad = stats[1].txBytes * 8.0 / (stats[1].timeLastTxPacket.GetSeconds () - stats[1].timeFirstTxPacket.GetSeconds ()) / 1000000;
+    double throughput = stats[1].rxBytes * 8.0 / (stats[1].timeLastRxPacket.GetSeconds () - stats[1].timeFirstRxPacket.GetSeconds ()) / 1000000;
+    uint32_t totalPacketsThr = DynamicCast<PacketSink> (sinkApps.Get(0))->GetTotalRx ();
+    double avgGoodput = totalPacketsThr * 8 / (simTime * 1000000.0); //Mbit/s
+
+    // Display the statistics
+    std::cout << std::endl << "*** Flow monitor statistics (distance = " << distance
+                << ")***" << std::endl;
+    std::cout << "  Tx Packets/Bytes:   " << stats[1].txPackets
+                << " / " << stats[1].txBytes << std::endl;
+    std::cout << "  Offered Load: " << offeredLoad << " Mbps" << std::endl;
+    std::cout << "  Rx Packets/Bytes:   " << stats[1].rxPackets
+                << " / " << stats[1].rxBytes << std::endl;
+    std::cout << "  Throughput: " << throughput << " Mbps" << std::endl;
+    std::cout << "  Average Goodput: " << avgGoodput << " Mbps" << std::endl;
+
+    monitor->SerializeToXmlFile("throughputAnalysis.csv", true, true);
+
+    // Stop simulation
     Simulator::Destroy ();
     NS_LOG_INFO ("Done.");
 }
